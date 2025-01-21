be0faa1f5953512d29c81be0ef146271364f40d4
David Rosenstein <mineawesomeman@gmail.com>
2025-01-21 14:15:17 -0500

    final things

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pizzeria-memories
  Bot to remind people of old messages
+
+## Setup
+1. clone repo
+2. add .env with your discord token (key is `DISCORD_TOKEN`)
+3. add your message tar
+4. run `setup.sh`
+
+## Run
+run `run.sh`
--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #/bin/bash
 
+echo Installing Required Libraries
+sudo apt install pip
+sudo apt install python3.11-venv
+
+echo Setting up Python Enviroment
 # create python enviroment
 python3 -m venv ./venv
 # install libraries
 ./venv/bin/pip install -r requirements.txt
+
+echo Unpacking Messages
+tar -xf messages.tar.gz
